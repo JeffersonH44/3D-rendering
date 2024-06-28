8b739659bf82421beb7254da5c79147c46444dc5
Jefferson Hernandez <jeffersonh44@gmail.com>
2024-06-28 17:40:00 -0500

    working version of a red squared window

--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,11 @@ fn sdlPanic() noreturn {
     @panic(std.mem.sliceTo(str, 0));
 }
 
-fn initialize_window()!void {
+fn initialize_game_loop()!void {
     if (SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING) != 0) {
         sdlPanic();
     }
+    defer SDL.SDL_Quit();
 
     // Create a window
     const window = SDL.SDL_CreateWindow(
@@ -19,19 +20,46 @@ fn initialize_window()!void {
         800, 
         600, 
         SDL.SDL_WINDOW_BORDERLESS
-    ).?;
+    ) orelse sdlPanic();
+    defer _ = SDL.SDL_DestroyWindow(window);
 
     const renderer = SDL.SDL_CreateRenderer(
         window, 
         -1, 
-        0).?;
+        0
+    ) orelse sdlPanic();
+    defer _ = SDL.SDL_DestroyRenderer(renderer);
 
+    mainLoop: while (true) {
+        var ev: SDL.SDL_Event = undefined;
+        while (SDL.SDL_PollEvent(&ev) != 0) {
+            switch (ev.type) {
+                SDL.SDL_QUIT => break :mainLoop,
+                SDL.SDL_KEYDOWN => {
+                    switch (ev.key.keysym.sym) {
+                        SDL.SDLK_ESCAPE => break :mainLoop,
+                        else => {},
+                    }
+                },
+                else => {},
+            }
+        }
+
+        _ = SDL.SDL_SetRenderDrawColor(
+            renderer,
+            255, 
+            0,
+            0, 
+            255
+        );
+        _ = SDL.SDL_RenderClear(renderer);
+        SDL.SDL_RenderPresent(renderer);
+    }
 }
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    initialize_window();
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    try initialize_game_loop();
 }
 
 test "simple test" {
